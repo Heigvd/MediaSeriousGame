1b6feea810cb0644268e928c4b00e72f1aeae1d0
Mikkel Vestergaard <56039404+TehAwol@users.noreply.github.com>
2024-06-19 12:14:17 +0200

    Misc fixes, add utils for basic scripting

---
+++ gameModel/libs/ClientScript/utils.ts
@@ -0,0 +1,27 @@
+/**
+ * Various utility functions to format or convert variables for display
+ */
+
+/**
+ * Convert number to Swiss currency format (ignoring cents)
+ */
+export function convertToCurrency(amount: number): string {
+  return new Intl.NumberFormat('fr-CH', { style: 'currency', currency: 'CHF', minimumFractionDigits: 0 }).format(
+    amount,
+  );
+}
+
+/**
+ * Small hack to toggle active class on navigation buttons
+ */
+type Page = 'accueil' | 'mail' | 'actions' | 'historique';
+
+export let currentPage: Page = "accueil";
+
+export function setCurrentPage(page: Page) {
+  currentPage = page;
+}
+
+export function getCurrentPage(): Page {
+  return currentPage;
+}
